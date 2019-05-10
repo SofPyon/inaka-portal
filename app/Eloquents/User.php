e922629a0f18626d9882c4d184b93a05500e2770
SofPyon <sofpyon@hrgrweb.com>
2019-05-10 14:56:07 +0900

    App\Eloquent\User → App\Eloquents\User

--- app/Eloquents/User.php
+++ app/Eloquents/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Eloquent;
+namespace App\Eloquents;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Eloquent\User::class,
+            'model' => App\Eloquents\User::class,
         ],
 
         // 'users' => [
