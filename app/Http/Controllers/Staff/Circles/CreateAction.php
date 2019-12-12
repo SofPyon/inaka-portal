16fd42ce5409933bbd0c4b636ee3a87ab8b08fcd
SofPyon <sofpyon@hrgrweb.com>
2019-12-12 11:19:09 +0900

    phpcbf でコード整形 #25

--- app/Http/Controllers/Staff/Circles/CreateAction.php
+++ app/Http/Controllers/Staff/Circles/CreateAction.php
@@ -26,7 +26,7 @@ class CreateAction extends Controller
             'regex'         => '学籍番号を確認してください',
         ];
 
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'name'      => ['required', 'unique:circles,name', 'max:255'],
             'leader'    => ['nullable', 'regex:/^[0-9a-z ]*$/', 'exists:users,student_id'],
             'members'   => ['nullable', 'regex:/^[0-9a-z (\r\n)]*$/'],
@@ -34,29 +34,26 @@ class CreateAction extends Controller
 
         $members_id = str_replace(["\r\n", "\r", "\n"], "\n", $request->members);
         $members_id = explode("\n", $members_id);
-        $members_id = array_filter( $members_id, "strlen") ;
+        $members_id = array_filter($members_id, "strlen") ;
 
         // members 存在チェック
         $notregister = null;
         $members = [];
-        foreach( $members_id as $member ){
+        foreach ($members_id as $member) {
             $member_id = $member;
             $member = $this->user->getUserByStudentId($member);
-            if (empty($member))
-            {
-                $notregister .= $member_id.' ';
+            if (empty($member)) {
+                $notregister .= $member_id . ' ';
             }
             array_push($members, $member);
         }
-        $validator->after(function ($validator) use ($notregister){
-            if (!empty($notregister))
-            {
+        $validator->after(function ($validator) use ($notregister) {
+            if (!empty($notregister)) {
                 $validator->errors()->add('members', '未登録：' . $notregister);
             }
         });
 
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return redirect()
             ->route('staff.circles.create')
             ->withErrors($validator)
@@ -72,12 +69,10 @@ class CreateAction extends Controller
         ]);
         $circle->users()->detach();
         $leader = $this->user->getUserByStudentId($request->leader);
-        $leader->circles()->attach(1, ['circle_id' => $circle->id, 'user_id' => $leader->id, 'is_leader' => True]);
-        foreach($members as $member)
-        {
-            $member->circles()->attach(1, ['circle_id' => $circle->id, 'user_id' => $member->id, 'is_leader' => False]);
+        $leader->circles()->attach(1, ['circle_id' => $circle->id, 'user_id' => $leader->id, 'is_leader' => true]);
+        foreach ($members as $member) {
+            $member->circles()->attach(1, ['circle_id' => $circle->id, 'user_id' => $member->id, 'is_leader' => false]);
         }
-        return redirect('/home_staff/circles/read/'. $circle->id);
-        
+        return redirect('/home_staff/circles/read/' . $circle->id);
     }
 }
--- app/Http/Controllers/Staff/Circles/StoreAction.php
+++ app/Http/Controllers/Staff/Circles/StoreAction.php
@@ -27,37 +27,34 @@ class StoreAction extends Controller
             'regex'         => '学籍番号を確認してください',
         ];
 
-        $validator = Validator::make($request->all(),[
-            'name'      => ['required', 'unique:circles,name,'.$circle->id.',id', 'max:255'],
+        $validator = Validator::make($request->all(), [
+            'name'      => ['required', 'unique:circles,name,' . $circle->id . ',id', 'max:255'],
             'leader'    => ['nullable', 'regex:/^[0-9a-z ]*$/', 'exists:users,student_id'],
             'members'   => ['nullable', 'regex:/^[0-9a-z (\r\n)]*$/'],
         ], $messages);
 
         $members_id = str_replace(["\r\n", "\r", "\n"], "\n", $request->members);
         $members_id = explode("\n", $members_id);
-        $members_id = array_filter( $members_id, "strlen") ;
+        $members_id = array_filter($members_id, "strlen") ;
 
         // members 存在チェック
         $notregister = null;
         $members = [];
-        foreach( $members_id as $member ){
+        foreach ($members_id as $member) {
             $member_id = $member;
             $member = $this->user->getUserByStudentId($member);
-            if (empty($member))
-            {
-                $notregister .= $member_id.' ';
+            if (empty($member)) {
+                $notregister .= $member_id . ' ';
             }
             array_push($members, $member);
         }
-        $validator->after(function ($validator) use ($notregister){
-            if (!empty($notregister))
-            {
+        $validator->after(function ($validator) use ($notregister) {
+            if (!empty($notregister)) {
                 $validator->errors()->add('members', '未登録：' . $notregister);
             }
         });
 
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return redirect()
                 ->route('staff.circles.edit', $circle)
                 ->withErrors($validator)
@@ -70,13 +67,11 @@ class StoreAction extends Controller
         $circle->updated_by = Auth::id();
         $circle->users()->detach();
         $leader = $this->user->getUserByStudentId($request->leader);
-        $leader->circles()->attach(1, ['circle_id' => $circle->id, 'user_id' => $leader->id, 'is_leader' => True]);
-        foreach($members as $member)
-        {
-            $member->circles()->attach(1, ['circle_id' => $circle->id, 'user_id' => $member->id, 'is_leader' => False]);
+        $leader->circles()->attach(1, ['circle_id' => $circle->id, 'user_id' => $leader->id, 'is_leader' => true]);
+        foreach ($members as $member) {
+            $member->circles()->attach(1, ['circle_id' => $circle->id, 'user_id' => $member->id, 'is_leader' => false]);
         }
         $circle->save();
-        return redirect('/home_staff/circles/read/'. $circle->id);
-        
+        return redirect('/home_staff/circles/read/' . $circle->id);
     }
 }
