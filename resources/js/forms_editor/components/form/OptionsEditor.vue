329ec55afd4793b2661cfe859da5bc116c28ce70
SofPyon <sofpyon@hrgrweb.com>
2019-12-14 08:08:42 +0900

    フォームエディター用の選択肢編集画面っぽいコンポーネントを作成 #36

---
+++ resources/js/forms_editor/components/form/OptionsEditor.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul v-if="options">
+      <li v-for="option in options" :key="option.id">
+        {{ option.value }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      // {id, value, priority}
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
