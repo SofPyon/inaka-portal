53227d1756ca5dfd6248742888c206a705ed1ad2
hosakou <hosa_kou@hotmail.co.jp>
2019-12-16 22:43:03 +0900

    登録チェッカーの結果表示を追加

--- app/Http/Controllers/Staff/Users/CheckAction.php
+++ app/Http/Controllers/Staff/Users/CheckAction.php
@@ -8,8 +8,18 @@ use App\Eloquents\User;
 
 class CheckAction extends Controller
 {
-    public function __invoke()
+    public function __construct(User $user)
     {
-        return ;
+        $this->user = $user;
+    }
+
+    public function __invoke(Request $request)
+    {
+        $user = $this->user->firstByStudentId($request->student_id);
+
+        // checker.blade.php の キャンセルボタン のリンクは Laravel以降後に修正
+        return view('staff.users.checker')
+            ->with('user', $user)
+            ->with('student_id', $request->student_id);
     }
 }
--- resources/views/staff/users/checker.blade.php
+++ resources/views/staff/users/checker.blade.php
@@ -5,22 +5,36 @@
 @section('main')
 <div class="card mb-3">
     <div class="card-header">ユーザー登録チェッカー</div>
-        <div class="card-body">
-            <form method="post" class="form-inline" action="{{ route('staff.users.check') }}">
-                <div class="form-group">
-                    @csrf
-                    <input type="text" class="form-control" name="student_id" placeholder="学籍番号">
-                    <button type="submit" class="btn btn-primary">確認</button>
-                </div>
-            </form>
-        </div>
+    <div class="card-body">
+        <form method="post" class="form-inline" action="{{ route('staff.users.check') }}">
+            <div class="form-group">
+                @csrf
+                <input type="text" class="form-control" name="student_id" placeholder="学籍番号">
+                <button type="submit" class="btn btn-primary ml-1">確認</button>
+                <a href="{{ url('/home_staff') }}" class="btn btn-light ml-1">キャンセル</a>
+            </div>
+        </form>
     </div>
 </div>
-@if (isset($user))
-    <div class="card mb-3">
-        <div class="card-body">
-            
-        </div>
+
+@if (isset($student_id))
+<div class="card {{ isset($user) ? $user->areBothEmailsVerified() ? 'border-success' : 'border-warning' : 'border-danger' }}">
+    <div class="card-header lead text-uppercase text-monospace">{{ $student_id }}</div>
+    <div class="card-body">
+        @if (isset($user))
+            <h4 class="{{ $user->areBothEmailsVerified() ? 'text-success' : 'text-warning' }} mb-1 card-title">{{ $user->areBothEmailsVerified() ? '登録済み' : 'メールの認証が済んでいません' }}</h4>
+            @if (!$user->hasVerifiedEmail())
+                <p class="text-muted mb-1">連絡用メール未認証</p>
+            @endif
+            @if (!$user->hasVerifiedUnivemail())
+                <p class="text-muted mb-1">学校発行のメール未認証</p>
+            @endif
+            <h5 class="card-title">{{ $user->name }}（{{ $user->name_yomi }}）</h5>
+            <a href="{{ url('/home_staff/users/read/' . $user->id)}}" class="btn btn-primary" role="button"><i class="fa fa-eye mr-1" aria-hidden="true"></i>詳細</a>
+        @else
+            <p class="text-danger lead">未登録</p>
+        @endif
     </div>
+</div>
 @endif
 @endsection
