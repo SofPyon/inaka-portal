b7f2e85736e45c7b5c85f5690d623769cda8df02
SofPyon <sofpyon@hrgrweb.com>
2019-12-17 12:37:09 +0900

    iPhone X対応するためのmeta設定追加 #106

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
     <title>@yield('title')</title>
 
     @prepend('css')
