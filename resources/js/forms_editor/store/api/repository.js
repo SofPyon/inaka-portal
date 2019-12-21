e9d202ceb58e610254589660aa51b5442a037d3b
SofPyon <sofpyon@hrgrweb.com>
2019-12-21 11:28:55 +0900

    とりあえず X-CSRF-TOKEN を meta タグの内容からとってきて送る #126

--- resources/js/forms_editor/store/api/repository.js
+++ resources/js/forms_editor/store/api/repository.js
@@ -5,11 +5,13 @@ import { SET_SAVING, SET_SAVED, ENQUEUED, DEQUEUED, SET_ERROR } from '../status'
 const baseURL = JSON.parse(
   document.querySelector('#forms-editor-config').dataset.apiBaseUrl
 )
+const token = document.head.querySelector('meta[name="csrf-token"]').content
 
 const axios = Axios.create({
   baseURL,
   headers: {
-    'X-Requested-With': 'XMLHttpRequest'
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-CSRF-TOKEN': token
   }
 })
 
