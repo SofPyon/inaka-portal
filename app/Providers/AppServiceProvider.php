4d08efa873087b80d8172b3b2fdd476532e510e0
SofPyon <sofpyon@hrgrweb.com>
2019-12-31 18:46:12 +0900

    Bladeに関する設定をBladeServiceProviderへ移動(v2) #150

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,28 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // スタッフページかどうかを Blade 上で判断できるようにする
-        // @staffpage 〜 @endstaffpage
-        // の中は、スタッフページの場合のみ表示される
-        Blade::if('staffpage', function () {
-            return strpos(request()->path(), 'staff') !== 0;
-        });
-
-        // 渡された引数の文字列をMarkdownとして解釈し、
-        // HTMLに変換した文字列を表示する
-        Blade::directive('markdown', function ($expression) {
-            return "<?php echo App\Services\Utils\ParseMarkdownService::render($expression); ?>";
-        });
-
-        // 渡された引数の文字列を先頭100文字のみのこし、
-        // 残りを「...」で省略する
-        Blade::directive('summary', function ($expression) {
-            return "<?php echo e(App\Services\Utils\FormatTextService::summary($expression)); ?>";
-        });
-
-        // 渡された引数の日付文字列をY年n月d日(曜日) H:i 形式の日付文字列にする
-        Blade::directive('datetime', function ($expression) {
-            return "<?php echo e(App\Services\Utils\FormatTextService::datetime($expression)); ?>";
-        });
+        //
     }
 }
