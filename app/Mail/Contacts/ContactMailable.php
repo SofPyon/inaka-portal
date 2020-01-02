5b2cee6c00760c21bc3ee22739e920ce893c03b7
SofPyon <sofpyon@hrgrweb.com>
2020-01-02 11:42:48 +0900

    お問い合わせメールを送信するためのサービスとMailableを作成 #133

---
+++ app/Mail/Contacts/ContactMailable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail\Contacts;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Eloquents\Circle;
+use App\Eloquents\User;
+
+class ContactMailable extends Mailable
+{
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('view.name');
+    }
+}
---
+++ app/Services/Contacts/ContactsService.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Auth;
+
+use Illuminate\Support\Facades\Mail;
+use App\Eloquents\Circle;
+use App\Eloquents\User;
+
+class ContactsService
+{
+    /**
+     * お問い合わせを作成する
+     *
+     * @param Circle|null $circle お問い合わせ対象の団体
+     * @param User $user お問い合わせを作成したユーザー
+     * @param string $body お問い合わせ本文
+     * @return bool
+     */
+    public function create(?Circle $circle, User $user, string $body)
+    {
+    }
+}
