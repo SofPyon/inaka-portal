446d3413e894fafd48cf09ae09330f7416ecb840
hosakou <hosa_kou@hotmail.co.jp>
2020-01-09 18:36:07 +0900

    ボタンの色を変更

--- resources/js/forms_editor/components/EditorHeader.vue
+++ resources/js/forms_editor/components/EditorHeader.vue
@@ -26,13 +26,15 @@
       <a class="btn btn-link" :href="preview_url" target="_blank">プレビュー</a>
       <span v-if="is_public">
         <span class="badge badge-primary mr-2">公開</span>
-        <button class="btn btn-primary" :disabled="is_saving">
+        <button class="btn btn-danger" :disabled="is_saving">
           非公開にする
         </button>
       </span>
       <span v-else>
         <span class="badge badge-danger mr-2">非公開</span>
-        <button class="btn btn-primary" :disabled="is_saving">公開する</button>
+        <button class="btn btn-primary" :disabled="is_saving">
+          公開する
+        </button>
       </span>
     </div>
   </header>
