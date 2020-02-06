4fc487724d4a672b703926a63ef4e7dfa0e6122a
SofPyon <sofpyon@hrgrweb.com>
2020-02-06 16:52:25 +0900

    ページ切り替え時のフォーム改善 #206

--- resources/js/v2/vue-turbolinks.js
+++ resources/js/v2/vue-turbolinks.js
@@ -7,15 +7,12 @@
  * MIT License
  */
 
-// リンククリック時にレイアウトが一瞬崩れたように見えてしまうため、
-// $destory は実行しない。
-// そのため、Vue の destroyed ライフサイクルメソッドは利用できない
-// function handleVueDestructionOn(turbolinksEvent, vue) {
-//   document.addEventListener(turbolinksEvent, function teardown() {
-//     vue.$destroy();
-//     document.removeEventListener(turbolinksEvent, teardown)
-//   })
-// }
+function handleVueDestructionOn(turbolinksEvent, vue) {
+  document.addEventListener(turbolinksEvent, function teardown() {
+    vue.$destroy()
+    document.removeEventListener(turbolinksEvent, teardown)
+  })
+}
 
 function plugin(Vue) {
   // Install a global mixin
@@ -24,17 +21,31 @@ function plugin(Vue) {
       // If this is the root component, we want to cache the original element contents to replace later
       // We don't care about sub-components, just the root
       if (this == this.$root && this.$el) {
-        // const destroyEvent =
-        // this.$options.turbolinksDestroyEvent || 'turbolinks:visit'
-        // handleVueDestructionOn(destroyEvent, this)
+        const destroyEvent =
+          this.$options.turbolinksDestroyEvent || 'turbolinks:visit'
+        handleVueDestructionOn(destroyEvent, this)
         this.$originalEl = this.$el.outerHTML
       }
     },
 
     destroyed() {
-      // We only need to revert the html for the root component
+      // 以下のコメントアウト部分 : We only need to revert the html for the root component
       if (this == this.$root && this.$el) {
-        this.$el.outerHTML = this.$originalEl
+        // ページ移動時、レイアウトが崩れるのを防ぐため、コメントアウト
+        // this.$el.outerHTML = this.$originalEl
+
+        // Turbolinks のキャッシュが表示されてから、サーバーのコンテンツが表示されるまでの間に
+        // input タグの内容が変わってしまうと、サーバーのコンテンツが表示された際に、
+        // 入力された内容が消えてしまうため、キャッシュ表示時はユーザーに
+        // 文字入力などをさせないようにする
+        const inputs = document.querySelectorAll(
+          'input, select, textarea, button'
+        )
+        /* eslint-disable no-restricted-syntax */
+        for (const input of inputs) {
+          input.disabled = 'disabled'
+        }
+        /* eslint-enable */
       }
     }
   })
