f85fe67bf392e7f82bd89eed39c622926a403d55
SofPyon <sofpyon@hrgrweb.com>
2020-02-19 01:22:27 +0900

    試し #164

--- tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
+++ tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
@@ -15,6 +15,8 @@ class LoginControllerTest extends TestCase
     {
         $response = $this->json('get', route('login'));
 
+        dd($response->content());
+
         $response->assertStatus(200);
         $response->assertViewIs('v2.auth.login');
     }
