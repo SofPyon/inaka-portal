33d49a2627b053dea7659c88099766ec31b7af6a
SofPyon <sofpyon@hrgrweb.com>
2020-02-22 01:19:15 +0900

    スタッフモードの配布資料管理からダウンロードする処理もLaravelへ移行 #240

--- app/Http/Controllers/Documents/ShowAction.php
+++ app/Http/Controllers/Documents/ShowAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Documents;
 
+use Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Eloquents\Document;
@@ -10,7 +11,7 @@ class ShowAction extends Controller
 {
     public function __invoke(Document $document)
     {
-        if (! $document->is_public || empty($document->filename)) {
+        if (!$document->is_public && (!Auth::check() || !Auth::user()->is_staff)) {
             abort(404);
             return;
         }
