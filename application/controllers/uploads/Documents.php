a2e22f7f916272eba21e8c252823ee1cc956d4a8
SofPyon <sofpyon@hrgrweb.com>
2020-02-22 00:30:53 +0900

    配布資料ダウンロード機能をLaravel化したが、スタッフモードのための対応は未完了なので、その旨コメント #240

--- application/controllers/uploads/Documents.php
+++ application/controllers/uploads/Documents.php
@@ -6,13 +6,13 @@ require_once __DIR__. '/Uploads_base_controller.php';
  */
 class Documents extends Uploads_base_controller
 {
+    // このコードは、スタッフモードの「配布資料管理」ページからファイルをダウンロードする際に
+    // アクセスされるので、スタッフモードの Laravel 化が完了するまで消さないこと
     public function index($document_id = null)
     {
         $document = $this->documents->get_document_by_document_id($document_id);
 
-        if ($document !== false && ( (int)$document->is_public === 1 ||
-        (!empty($this->_get_login_user()) && (int)$this->_get_login_user()->is_staff === 1) )
-        ) {
+        if ($document !== false && !empty($this->_get_login_user()) && (int)$this->_get_login_user()->is_staff === 1) {
           // basename: ディレクトリ・トラバーサル脆弱性対策
             if (empty($document->filename)) {
                 $this->_uploads_forbidden();
