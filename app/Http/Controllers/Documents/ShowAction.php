aca8acd44918f034b6a4a2c2b47947bf9d470447
SofPyon <sofpyon@hrgrweb.com>
2020-02-22 00:18:21 +0900

    配布資料ダウンロード部分をLaravel化 #240

--- app/Http/Controllers/Documents/ShowAction.php
+++ app/Http/Controllers/Documents/ShowAction.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers\Documents;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Eloquents\Document;
 
 class ShowAction extends Controller
 {
-    public function __invoke()
+    public function __invoke(Document $document)
     {
+        if (! $document->is_public || empty($document->filename)) {
+            abort(404);
+            return;
+        }
+
+        // basename: ディレクトリ・トラバーサル脆弱性対策
+        $path = config('portal.codeigniter_upload_dir') . '/documents/' . basename($document->filename);
+
+        return response()->file($path);
     }
 }
