e25b445b5e16b0f45af22eb8f7ac76568842812a
SofPyon <sofpyon@hrgrweb.com>
2020-02-21 23:41:51 +0900

    配布資料ダウンロード用のActionを用意 #240

---
+++ app/Http/Controllers/Documents/ShowAction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Documents;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ShowAction extends Controller
+{
+    public function __invoke()
+    {
+    }
+}
